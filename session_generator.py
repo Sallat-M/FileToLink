89882e4bf4c6e5494e10dff71be0bf2f749f15c2
hodij88913 <149895467+hodij88913@users.noreply.github.com>
2023-11-06 22:49:50 +0530

    Update session_generator.py

--- session_generator.py
+++ session_generator.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
 
-api_id = input("Enter API_ID:\n")
-api_hash = input("Enter API_HASH:\n")
-bot_token = input("Enter BOT_TOKEN:\n")
+api_id = input("Enter API_ID:\n'24335028')
+api_hash = input("Enter API_HASH:\n'b204ec833fb451fb913fc8e683b232d0')
+bot_token = input("Enter BOT_TOKEN:\n '6247730826:AAGICcluDNXCsWOKJuiIRn-ulrBau5I-tmY')
 
 client = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 client.start()
